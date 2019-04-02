7df70dff1260faa0f071b0ec42240437f5f37951
wniedzwiedz <33662295+wniedzwiedz@users.noreply.github.com>
2019-04-02 12:48:40 +0200

    PrimeNumber

---
+++ src/main/java/pl/opitz/consulting/caseweek/tasks/PrimeNumberTask.java
@@ -0,0 +1,36 @@
+package pl.opitz.consulting.caseweek.tasks;
+
+import pl.opitz.consulting.caseweek.model.PrimeNumberTaskRequest;
+import pl.opitz.consulting.caseweek.model.PrimeNumberTaskResponse;
+
+public class PrimeNumberTask implements Task<PrimeNumberTaskRequest, PrimeNumberTaskResponse> {
+
+    @Override
+    public PrimeNumberTaskResponse resolveTask(PrimeNumberTaskRequest request) {
+
+        int n = request.getNumber();
+
+        int remainder;
+        boolean isPrime=true;
+
+        for(int i=2;i<=n/2;i++)
+        {
+            remainder=n%i;
+            if(remainder==0)
+            {
+                isPrime=false;
+                break;
+            }
+        }
+
+
+        if(isPrime) {
+            return new PrimeNumberTaskResponse(true);
+        }
+
+
+        return new PrimeNumberTaskResponse(false);;
+    }
+}
+
+
