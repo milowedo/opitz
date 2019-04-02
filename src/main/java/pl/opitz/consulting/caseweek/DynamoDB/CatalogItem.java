d033dc0e2528f7de592ede5702796cee1aef4238
Weronika Niedzwidz <wmniedzwiedz@gmail.com>
2019-04-02 14:39:59 +0200

    Klasa do zapisu do DynamoDB

--- src/main/java/pl/opitz/consulting/caseweek/DynamoDB/CatalogItem.java
+++ src/main/java/pl/opitz/consulting/caseweek/DynamoDB/CatalogItem.java
@@ -1,10 +1,14 @@
 package pl.opitz.consulting.caseweek.DynamoDB;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
 @DynamoDBTable(tableName="Results")
-public class CatalogItem {
+public class CatalogItem{
+    static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
 
     private String task;
     private String result;
@@ -16,6 +20,17 @@ public class CatalogItem {
     @DynamoDBAttribute(attributeName="RESULT")
     public String getResult() {return result; }
     public void setResult(String t) { this.result = t; }
+
+    @Override
+    public String toString() {
+        return "TASK= "+task+" RESULT= "+result;
+    }
+
+    CatalogItem(String r, String t){
+        task=t;
+        result=r;
+    }
+
 
 
 }
---
+++ src/main/java/pl/opitz/consulting/caseweek/DynamoDB/DAO.java
@@ -0,0 +1,13 @@
+package pl.opitz.consulting.caseweek.DynamoDB;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+
+public class DAO {
+
+    private static void save(CatalogItem a){
+        CatalogItem item = new CatalogItem(a.getResult(),a.getResult());
+
+        DynamoDBMapper mapper = new DynamoDBMapper(a.client);
+        mapper.save(item);
+    }
+}
